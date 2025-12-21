2a605fb175b7028212ec4c13d61900de12f89531
natively.dev <api@natively.dev>
2025-12-21 07:18:39 +0000

    fix: Prevent context access on non-web platforms in EditableElement

--- babel-plugins/react/EditableElement_.tsx
+++ babel-plugins/react/EditableElement_.tsx
@@ -1,3 +1,4 @@
+
 /* eslint-disable */
 
 // @eslint-ignore-file
@@ -34,6 +35,15 @@ const toArray = (object: T | T[]): T[] => {
 };
 
 export default function EditableElement_(_props: PropsWithChildren<any>) {
+  const { children } = _props;
+  const { props } = children;
+
+  // If we are not running in the web the windows will causes
+  // issues hence editable mode is not enabled.
+  if (Platform.OS !== "web") {
+    return cloneElement(children, props);
+  }
+
   const {
     editModeEnabled,
     selected,
@@ -43,15 +53,6 @@ export default function EditableElement_(_props: PropsWithChildren<any>) {
     pushHovered,
     popHovered,
   } = useContext(EditableContext);
-
-  const { children } = _props;
-  const { props } = children;
-
-  // If we are not running in the web the windows will causes
-  // issues hence editable mode is not enabled.
-  if (Platform.OS !== "web") {
-    return cloneElement(children, props);
-  }
 
   const type = getType(children);
   const __sourceLocation = props.__sourceLocation;
